f4c4ee60f2c1383f4980781fec64b3c223066eab
Leandro Santos <161133257+leandroibov@users.noreply.github.com>
2026-02-15 01:29:37 +0000

    Add files via upload

--- 4.2.ephemeral_old.sh
+++ 4.2.ephemeral_old.sh
@@ -458,6 +458,329 @@ echo "Reboot to verify...";
 }
 
 
+anti_cold_boot()
+{
+echo
+# Script to install dracut ram-wipe module
+# from qubes forum post: https://forum.qubes-os.org/t/ram-wipe-in-dom0-protection-against-cold-boot-attack-in-qubes/39375
+
+echo "Creating dracut ram-wipe module directories and files..."
+
+# Create module directory
+mkdir /usr/lib/dracut/modules.d/40ram-wipe/
+
+# module-setup.sh
+cat > /usr/lib/dracut/modules.d/40ram-wipe/module-setup.sh << 'EOF'
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+## Copyright (C) 2023 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
+## See the file COPYING for copying conditions.
+
+# called by dracut
+check() {
+   require_binaries sync || return 1
+   require_binaries sleep || return 1
+   require_binaries dmsetup || return 1
+   return 0
+}
+
+# called by dracut
+depends() {
+   return 0
+}
+
+# called by dracut
+install() {
+   inst_simple "/usr/libexec/ram-wipe/ram-wipe-lib.sh" "/lib/ram-wipe-lib.sh"
+   inst_multiple sync
+   inst_multiple sleep
+   inst_multiple dmsetup
+   inst_hook shutdown 40 "$moddir/wipe-ram.sh"
+   inst_hook cleanup 80 "$moddir/wipe-ram-needshutdown.sh"
+}
+
+# called by dracut
+installkernel() {
+   return 0
+}
+EOF
+
+chmod +x /usr/lib/dracut/modules.d/40ram-wipe/module-setup.sh
+
+# wipe-ram-needshutdown.sh
+cat > /usr/lib/dracut/modules.d/40ram-wipe/wipe-ram-needshutdown.sh << 'EOF'
+#!/bin/sh
+
+## Copyright (C) 2023 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
+## See the file COPYING for copying conditions.
+
+type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
+
+. /lib/ram-wipe-lib.sh
+
+ram_wipe_check_needshutdown() {
+   ## 'local' is unavailable in 'sh'.
+   #local kernel_wiperam_setting
+
+   kernel_wiperam_setting="$(getarg wiperam)"
+
+   if [ "$kernel_wiperam_setting" = "skip" ]; then
+      force_echo "wipe-ram-needshutdown.sh: Skip, because wiperam=skip kernel parameter detected, OK."
+      return 0
+   fi
+
+   true "wipe-ram-needshutdown.sh: Calling dracut function need_shutdown to drop back into initramfs at shutdown, OK."
+   need_shutdown
+
+   return 0
+}
+
+ram_wipe_check_needshutdown
+EOF
+
+chmod +x /usr/lib/dracut/modules.d/40ram-wipe/wipe-ram-needshutdown.sh
+
+# wipe-ram.sh
+cat > /usr/lib/dracut/modules.d/40ram-wipe/wipe-ram.sh << 'EOF'
+#!/bin/sh
+
+## Copyright (C) 2023 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
+## See the file COPYING for copying conditions.
+
+## Credits:
+## First version by @friedy10.
+## https://github.com/friedy10/dracut/blob/master/modules.d/40sdmem/wipe.sh
+
+## Use '.' and not 'source' in 'sh'.
+. /lib/ram-wipe-lib.sh
+
+drop_caches() {
+   sync
+   ## https://gitlab.tails.boum.org/tails/tails/-/blob/master/config/chroot_local-includes/usr/local/lib/initramfs-pre-shutdown-hook
+   ### Ensure any remaining disk cache is erased by Linux' memory poisoning
+   echo 3 > /proc/sys/vm/drop_caches
+   sync
+}
+
+ram_wipe() {
+   ## 'local' is unavailable in 'sh'.
+   #local kernel_wiperam_setting dmsetup_actual_output dmsetup_expected_output
+
+   ## getarg returns the last parameter only.
+   kernel_wiperam_setting="$(getarg wiperam)"
+
+   if [ "$kernel_wiperam_setting" = "skip" ]; then
+      force_echo "wipe-ram.sh: Skip, because wiperam=skip kernel parameter detected, OK."
+      return 0
+   fi
+
+   force_echo "wipe-ram.sh: RAM extraction attack defense... Starting RAM wipe pass during shutdown..."
+
+   drop_caches
+
+   force_echo "wipe-ram.sh: RAM wipe pass completed, OK."
+
+   ## In theory might be better to check this beforehand, but the test is
+   ## really fast.
+   force_echo "wipe-ram.sh: Checking if there are still mounted encrypted disks..."
+
+   ## TODO: use 'timeout'?
+   dmsetup_actual_output="$(dmsetup ls --target crypt 2>&1)"
+   dmsetup_expected_output="No devices found"
+
+   if [ "$dmsetup_actual_output" = "$dmsetup_expected_output" ]; then
+      force_echo "wipe-ram.sh: Success, there are no more mounted encrypted disks, OK."
+   elif [ "$dmsetup_actual_output" = "" ]; then
+      force_echo "wipe-ram.sh: Success, there are no more mounted encrypted disks, OK."
+   else
+      ## dracut should unmount the root encrypted disk cryptsetup luksClose during shutdown
+      ## https://github.com/dracutdevs/dracut/issues/1888
+      force_echo "\\
+wipe-ram.sh: There are still mounted encrypted disks! RAM wipe incomplete!
+
+debugging information:
+dmsetup_expected_output: '$dmsetup_expected_output'
+dmsetup_actual_output: '$dmsetup_actual_output'"
+      ## How else could the user be informed that something is wrong?
+      sleep 5
+   fi
+}
+
+ram_wipe
+EOF
+
+chmod +x /usr/lib/dracut/modules.d/40ram-wipe/wipe-ram.sh
+
+# dracut.conf.d
+cat > /usr/lib/dracut/dracut.conf.d/30-ram-wipe.conf << 'EOF'
+add_dracutmodules+=" ram-wipe "
+EOF
+
+# ram-wipe-lib.sh
+mkdir /usr/libexec/ram-wipe
+cat > /usr/libexec/ram-wipe/ram-wipe-lib.sh << 'EOF'
+#!/bin/sh
+
+## Copyright (C) 2023 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
+## See the file COPYING for copying conditions.
+
+## Based on:
+## /usr/lib/dracut/modules.d/99base/dracut-lib.sh
+if [ -z "$DRACUT_SYSTEMD" ]; then
+    force_echo() {
+        echo "<28>dracut INFO: $*" > /dev/kmsg
+        echo "dracut INFO: $*" >&2
+    }
+else
+    force_echo() {
+        echo "INFO: $*" >&2
+    }
+fi
+EOF
+
+chmod +x /usr/libexec/ram-wipe/ram-wipe-lib.sh
+
+# Update INITRAMFS
+dracut --verbose --force
+echo "ram-wipe module created successfully!"
+echo "Use a shortcut key like Control + Alt + Space to activate fast shutdown against physical attackers."
+echo "Manually configure the settings manager / Keyboard."
+echo "Dom0 does not support USB drives to create a USB kill switch like Tails OS does."
+echo
+
+# Just for testing at the beginning of the creation of the algorithm
+# Commented out now to be used in the future if something is modified to change the remove_anti_cold_boot() function with new hashes
+#remove : << 'END_COMMENT' and END_COMMENT to apply again...
+
+: << 'END_COMMENT'
+#echo "Hash of the files created"
+# Define the directory and files to check
+files=(
+    "/usr/lib/dracut/modules.d/40ram-wipe/module-setup.sh"
+    "/usr/lib/dracut/modules.d/40ram-wipe/wipe-ram-needshutdown.sh"
+    "/usr/lib/dracut/modules.d/40ram-wipe/wipe-ram.sh"
+    "/usr/lib/dracut/dracut.conf.d/30-ram-wipe.conf"
+    "/usr/libexec/ram-wipe/ram-wipe-lib.sh"
+)
+
+# Iterate through the files and calculate the SHA-256 hash
+for file in "${files[@]}"; do
+    if [[ -f "$file" ]]; then
+        hash=$(sha256sum "$file" | awk '{ print $1 }')
+        echo "File: $file"
+        echo "SHA-256 Hash: $hash"
+        echo "-----------------------------"
+    else
+        echo "File: $file does not exist."
+        echo "-----------------------------"
+    fi
+done
+END_COMMENT
+
+
+
+}
+
+
+remove_anti_cold_boot()
+{
+echo
+# Define the files and their expected hashes
+declare -A files_hashes
+files_hashes=(
+    ["/usr/lib/dracut/modules.d/40ram-wipe/module-setup.sh"]="246d9f7fb41d7f2361d0e086889a1570625c4468ee17ccf06fdd8c3044cb6049"
+    ["/usr/lib/dracut/modules.d/40ram-wipe/wipe-ram-needshutdown.sh"]="55be0355df0cdf9eb4f135602a87e2b47f6807e00baf24becea41a478064795d"
+    ["/usr/lib/dracut/modules.d/40ram-wipe/wipe-ram.sh"]="a1a121e7faaaf5b4a042a8a63887fdfd4e4891c9ccdb976bcafc0a228c0b1a48"
+    ["/usr/lib/dracut/dracut.conf.d/30-ram-wipe.conf"]="60463385aff5cf70d815a0b399f0c8142a8eef6b8a5e643da82e4a59e7fa73c4"
+    ["/usr/libexec/ram-wipe/ram-wipe-lib.sh"]="1fdd83475d59f1942492cac643d63a607339190c11bda4a401a51c34112a871d"
+)
+
+# Check each file's existence and hash
+all_exist=true
+all_match=true
+
+for file in "${!files_hashes[@]}"; do
+    if [[ -f "$file" ]]; then
+        # Calculate the SHA-256 hash
+        calculated_hash=$(sha256sum "$file" | awk '{ print $1 }')
+        expected_hash=${files_hashes[$file]}
+
+        echo "File: $file"
+        echo "Calculated Hash: $calculated_hash"
+        echo "Expected Hash: $expected_hash"
+
+        if [[ "$calculated_hash" != "$expected_hash" ]]; then
+            echo "Warning: Hash does not match for $file."
+            all_match=false
+        else
+            echo "Hash matches for $file."
+        fi
+        echo "-----------------------------"
+    else
+        echo "File: $file does not exist."
+        all_exist=false
+    fi
+done
+
+# Check the status of files and hashes
+if $all_exist; then
+    echo "All files exist."
+else
+    echo "Some files are missing."
+fi
+
+if $all_match; then
+    echo "All hashes correspond."
+else
+    echo "Some hashes do not correspond."
+fi
+
+echo "Anti cold boot attack modules are already installed."
+
+# Prompt to continue or cancel the operation
+read -p "Do you wish to continue or cancel the operation? (type 'y' or 'n'): " user_input
+echo
+
+if [[ "$user_input" == "y" ]]; then
+    echo "Continuing the operation..."
+
+# Inform the user about the deletion process
+echo "Deleting Dracut Anti Cold Boot Attack Modules..."
+
+# List of directories and files to delete
+items=(
+    "/usr/lib/dracut/modules.d/40ram-wipe/"
+    "/usr/lib/dracut/modules.d/40ram-wipe/"  # Appears multiple times
+    "/usr/lib/dracut/modules.d/40ram-wipe/"
+    "/usr/lib/dracut/modules.d/40ram-wipe/"
+    "/usr/libexec/ram-wipe/"
+    "/usr/lib/dracut/dracut.conf.d/30-ram-wipe.conf"
+)
+
+# Loop through the items and delete each one
+for item in "${items[@]}"; do
+    if [[ -e "$item" ]]; then
+        echo "Deleting: $item"
+        sudo rm -rf "$item"
+    else
+        echo "Warning: $item does not exist or cannot be deleted."
+    fi
+done
+echo
+# Regenerate Dracut
+echo "Regenerating Dracut..."
+sudo dracut --verbose --force
+echo "Operation completed."
+
+else
+    echo "Operation canceled."
+fi
+
+}
+
+
 echo;
 echo "-----------------------";
 echo " Qubes Ephemeral Vault ";
@@ -472,7 +795,9 @@ check_root_ram
         echo "Menu:"
         echo "1. Set up Qubes dom0 for 100% RAM (Ephemeral)"
         echo "2. Restore original Qubes settings"
-        echo "3. Exit"
+        echo "3. Install Anti-Cold Boot Attack Dracut Modules"
+        echo "4. Remove Anti-Cold Boot Attack Dracut Modules for maintenance"
+        echo "5. Exit"
         read -p "Enter your choice (1-3): " choice
         
         case $choice in
@@ -483,6 +808,12 @@ check_root_ram
                 restore_original_qubes
                 ;;
             3)
+                anti_cold_boot
+                ;;
+            4)
+                remove_anti_cold_boot
+                ;;
+            5)
                 echo "Exiting the menu."
                 break
                 ;;
@@ -491,86 +822,3 @@ check_root_ram
                 ;;
         esac
     done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
